dcebe21946c3c39880fc4e31aad00f722e321d82
Kyle <kylevon1@gmail.com>
2025-01-31 19:26:52 +0100

    Michro app now has a login screen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/home/presentation/pages/home_page.dart';
+import '../../features/auth/presentation/pages/login_page.dart';
 
 class AppRouter {
   static final router = GoRouter(
-    initialLocation: '/',
+    initialLocation: '/login',
     routes: [
       GoRoute(
-        path: '/',
+        path: '/login',
+        builder: (context, state) => const LoginPage(),
+      ),
+      GoRoute(
+        path: '/home',
         builder: (context, state) => const HomePage(),
       ),
-      // Add more routes as needed
     ],
     errorBuilder: (context, state) => Scaffold(
       body: Center(
